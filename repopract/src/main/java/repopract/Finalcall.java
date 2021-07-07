d6dfdf4bdf8ab59ff53a5477a0ed6f31b312912a
prithviraj-jadeja <40898178+prithviraj-jadeja@users.noreply.github.com>
2021-07-07 15:37:36 +0530

    updated

--- repopract/src/main/java/repopract/Finalcall.java
+++ repopract/src/main/java/repopract/Finalcall.java
@@ -5,6 +5,9 @@ public class Finalcall {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("prithprint");
+		System.out.println("prhpr");
+		System.out.println("prith");
+		System.out.println("print");
 
 	}
 
